3b1942426deb1b7f5f04c2f6dee2a79cfc7d912e
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-25 17:02:38 -0300

    dev: Create searching sales model

---
+++ models/sales.js
@@ -0,0 +1,25 @@
+const database = require('../database');
+
+function getAllSales() {
+  const SELECT = 'SELECT sale_id, sales.`date`, product_id, quantity FROM sales_products ';
+  const INNER_JOIN = 'INNER JOIN sales ON sales_products.sale_id = sales.id;';
+
+  const QUERY = SELECT + INNER_JOIN;
+
+  return database.execute(QUERY);
+}
+
+function getSaleById(id) {
+  const SELECT = 'SELECT sales.`date`, product_id, quantity FROM sales_products ';
+  const INNER_JOIN = 'INNER JOIN sales ON sales_products.sale_id = sales.id';
+  const WHERE = 'WHERE sale_id = ?;';
+
+  const QUERY = SELECT + INNER_JOIN + WHERE;
+
+  return database.execute(QUERY, [id]);
+}
+
+module.exports = {
+  getAllSales,
+  getSaleById,
+};
