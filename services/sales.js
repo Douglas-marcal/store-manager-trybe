72d9a549fcbcfc0c9fc9178ad71064352c959abb
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-25 19:55:10 -0300

    dev: Create services for sales

---
+++ services/sales.js
@@ -0,0 +1,20 @@
+const salesModel = require('../models/sales');
+const { allSalesToCamelCase, saleByIdToCamelCase } = require('./utils/salesUtils');
+
+async function getSales(id = null) {
+  if (id) {
+    const [sales] = await salesModel.getSaleById();
+
+    const saleById = sales.map(saleByIdToCamelCase);
+
+    return saleById;
+  }
+
+  const [sales] = await salesModel.getAllSales();
+
+  const allSales = sales.map(allSalesToCamelCase);
+
+  return allSales;
+}
+
+module.exports = getSales;
