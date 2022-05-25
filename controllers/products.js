e6bbbb798e5b90cb44dd1af3c8a97f86026d8839
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-25 20:54:02 -0300

    dev: Create response to endpoint `products`

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllProducts } = require('../services/products');
+const { getAllProducts, getProductById } = require('../services/products');
 
 const routes = express.Router();
 
@@ -7,6 +7,16 @@ routes.get('/', async (_request, response) => {
   const [products] = await getAllProducts();
 
   response.status(200).json(products);
+});
+
+routes.get('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const [[product]] = await getProductById(id);
+
+  if (!product) return response.status(404).json({ message: 'Product not found' });
+
+  response.status(200).json(product);
 });
 
 module.exports = routes;
