6901a92d2ad22668ba7b3f3b6ab1d66fd64c3920
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-26 20:31:28 -0300

    dev: Create service to register a new product

--- services/products.js
+++ services/products.js
@@ -1,3 +1,4 @@
+const { MESSAGE } = require('../constants/messages');
 const productsModel = require('../models/products');
 
 function getProducts(id = null) {
@@ -6,6 +7,22 @@ function getProducts(id = null) {
   return productsModel.getAllProducts();
 }
 
+async function registerProduct(product) {
+  const { name, quantity } = product;
+
+  const [products] = await productsModel.getAllProducts();
+  const alreadyExistsProduct = products.some((productDatabase) => name === productDatabase.name);
+
+  if (alreadyExistsProduct) throw MESSAGE.ALREADY_EXISTS;
+
+  const { id } = await productsModel.registerProduct(product);
+
+  const newProduct = { id, name, quantity };
+
+  return newProduct;
+}
+
 module.exports = {
   getProducts,
+  registerProduct,
 };
