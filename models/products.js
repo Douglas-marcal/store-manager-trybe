f7546181058e8a7f0f93f9382ab91c66a0286ac9
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-26 20:14:04 -0300

    feat: Create a function to register a new product

--- models/products.js
+++ models/products.js
@@ -8,7 +8,24 @@ function getProductById(id) {
   return database.execute('SELECT * FROM products WHERE id = ?', [id]);
 }
 
+async function registerProduct(product) {
+  const { name, quantity } = product;
+
+  const QUERY = 'INSERT INTO products (name, quantity) VALUES (?, ?);';
+
+  const [productAdded] = await database.execute(QUERY, [name, quantity]);
+
+  const response = {
+    id: productAdded.insertId,
+    name,
+    quantity,
+  };
+
+  return response;
+}
+
 module.exports = {
   getAllProducts,
   getProductById,
+  registerProduct,
 };
