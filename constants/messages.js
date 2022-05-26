682d8e5d760e96e78b310a6beb7cee2404d3d141
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-25 23:05:30 -0300

    dev: Create a file to allocate all messages

---
+++ constants/messages.js
@@ -0,0 +1,12 @@
+const MESSAGE = {
+  SALE_NOT_FOUND: {
+    status: 404,
+    message: 'Sale not found',
+  },
+  PRODUCT_NOT_FOUND: {
+    status: 404,
+    message: 'Product not found', 
+  },
+};
+
+module.exports = { MESSAGE };
