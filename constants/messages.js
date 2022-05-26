81aea46da7be7a99908baa6664959af4f45026eb
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-26 19:56:17 -0300

    dev: Create messages to validate product

--- constants/messages.js
+++ constants/messages.js
@@ -9,6 +9,22 @@ const MESSAGE = {
     status: CODE.NOT_FOUND,
     message: 'Product not found', 
   },
+  NAME_REQUIRED: {
+    status: CODE.BAD_REQUEST,
+    message: '"name" is required',
+  },
+  QUANTITY_REQUIRED: {
+    status: CODE.BAD_REQUEST,
+    message: '"quantity" is required',
+  },
+  NAME_TOO_SHORT: {
+    status: CODE.UNPROCESSABLE_ENTITY,
+    message: '"name" length must be at least 5 characters long',
+  },
+  LOW_QUANTITY: {
+    status: CODE.UNPROCESSABLE_ENTITY,
+    message: '"quantity" must be greater than or equal to 1',
+  },
 };
 
 module.exports = { MESSAGE };
